dbf36cdb8630f99d90b83fdcd57b5f5a0ce5a334
Dmitriy <xburv@mail.ru>
2023-03-28 23:24:06 +0500

    Python

--- Погружение в Python/Seminar_05/1. Функция строка-путь до файла.py
+++ Погружение в Python/Seminar_05/1. Функция строка-путь до файла.py
@@ -3,3 +3,17 @@
 Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 """
 
+
+def split_file_path(file_path):
+    file_name = file_path.split('/')[-1]
+    file_parts = file_name.split('.')
+    if len(file_parts) == 1:
+        file_name = file_parts[0]
+        file_ext = ''
+    else:
+        file_name, file_ext = file_parts[0], file_parts[-1]
+    path = '/'.join(file_path.split('/')[:-1])
+    return path, file_name, file_ext
+
+
+print(split_file_path("/Users/xburv/Desktop/Разработчик/Python/file.txt"))
