04b73c4233f4a8a1223e530228e7fc4f83582fee
Dmitriy <xburv@mail.ru>
2023-03-29 20:21:09 +0500

    Python

--- Погружение в Python/Seminar_05/3. Генератор чисел Фибоначчи.py
+++ Погружение в Python/Seminar_05/3. Генератор чисел Фибоначчи.py
@@ -2,3 +2,14 @@
 Создайте функцию генератор чисел Фибоначчи
 """
 
+
+def fib():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+fib_gen = fib()
+for i in range(20):
+    print(next(fib_gen))
